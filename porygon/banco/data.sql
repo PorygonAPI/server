799e7367b94cda156b7a92ae9a2a78150c028c00
RuthMira <143049504+RuthMira@users.noreply.github.com>
2025-03-16 17:08:01 -0300

    feat  (#16): Criar tabela areas agricolas e exemplo para o banco

--- porygon/banco/data.sql
+++ porygon/banco/data.sql
@@ -2,3 +2,6 @@ INSERT INTO cargo (nome) VALUES ('Consultor'), ('Analista'), ('Administrador');
 INSERT INTO usuario (nome, senha, email, cargo_id) VALUES ('admin', '1234', 'admin@visiona.com', 3);
 INSERT INTO usuario (nome, senha, email, cargo_id) VALUES ('analista', '1234', 'analista@visiona.com', 2);
 INSERT INTO usuario (nome, senha, email, cargo_id) VALUES ('consultor', '1234', 'consultor@visiona.com', 1);
+INSERT INTO area_agricola (usuario_id, usuario_upgrade_id, usuario_aprovador_id, nome_fazenda, cultura, produtividade_ano, area, tipo_solo, cidade, estado, vetor_raiz, vetor_atualizado, vetor_aprovado, status) 
+VALUES 
+(1, NULL, NULL, 'Fazenda Exemplo', 'Soja', 3500.50, 150.75, 'Argiloso', 'Ribeirão Preto', 'SP','{"type": "Polygon", "coordinates": [[[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]]}', NULL,  NULL,'pendente');
