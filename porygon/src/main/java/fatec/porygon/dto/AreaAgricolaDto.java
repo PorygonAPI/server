f4f71568e657410843219b30b633dc0feaf6e0ed
RenatCMMendes <renato.mendes2@fatec.sp.gov.br>
2025-03-19 21:31:56 -0300

    feat(#15): trocado a formatacao de CamelCase para snake_case

--- porygon/src/main/java/fatec/porygon/dto/AreaAgricolaDto.java
+++ porygon/src/main/java/fatec/porygon/dto/AreaAgricolaDto.java
@@ -4,52 +4,52 @@ import fatec.porygon.enums.StatusArea;
 
 public class AreaAgricolaDto {
     private Long id;
-    private Long usuarioId;
-    private Long usuarioUpgradeId;
-    private Long usuarioAprovadorId;
-    private String nomeFazenda;
+    private Long usuario_id;
+    private Long usuario_upgrade_id;
+    private Long usuario_aprovador_id;
+    private String nome_fazenda;
     private String cultura;
-    private Double produtividadeAno;
+    private Double produtividade_ano;
     private Double area;
-    private String tipoSolo;
+    private String tipo_solo;
     private String cidade;
     private String estado;
-    private String vetorRaiz;
-    private String vetorAtualizado;
-    private String vetorAprovado;
+    private String vetor_raiz;
+    private String vetor_atualizado;
+    private String vetor_aprovado;
     private StatusArea status;
 
     // Getters
     public Long getId() { return id; }
-    public Long getUsuarioId() { return usuarioId; }
-    public Long getUsuarioUpgradeId() { return usuarioUpgradeId; }
-    public Long getUsuarioAprovadorId() { return usuarioAprovadorId; }
-    public String getNomeFazenda() { return nomeFazenda; }
+    public Long getusuario_id() { return usuario_id; }
+    public Long getusuario_upgrade_id() { return usuario_upgrade_id; }
+    public Long getusuario_aprovador_id() { return usuario_aprovador_id; }
+    public String getnome_fazenda() { return nome_fazenda; }
     public String getCultura() { return cultura; }
-    public Double getProdutividadeAno() { return produtividadeAno; }
+    public Double getprodutividade_ano() { return produtividade_ano; }
     public Double getArea() { return area; }
-    public String getTipoSolo() { return tipoSolo; }
+    public String gettipo_solo() { return tipo_solo; }
     public String getCidade() { return cidade; }
     public String getEstado() { return estado; }
-    public String getVetorRaiz() { return vetorRaiz; }
-    public String getVetorAtualizado() { return vetorAtualizado; }
-    public String getVetorAprovado() { return vetorAprovado; }
+    public String getvetor_raiz() { return vetor_raiz; }
+    public String getvetor_atualizado() { return vetor_atualizado; }
+    public String getvetor_aprovado() { return vetor_aprovado; }
     public StatusArea getStatus() { return status; }
 
     // Setters
     public void setId(Long id) { this.id = id; }
-    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
-    public void setUsuarioUpgradeId(Long usuarioUpgradeId) { this.usuarioUpgradeId = usuarioUpgradeId; }
-    public void setUsuarioAprovadorId(Long usuarioAprovadorId) { this.usuarioAprovadorId = usuarioAprovadorId; }
-    public void setNomeFazenda(String nomeFazenda) { this.nomeFazenda = nomeFazenda; }
+    public void setusuario_id(Long usuario_id) { this.usuario_id = usuario_id; }
+    public void setusuario_upgrade_id(Long usuario_upgrade_id) { this.usuario_upgrade_id = usuario_upgrade_id; }
+    public void setusuario_aprovador_id(Long usuario_aprovador_id) { this.usuario_aprovador_id = usuario_aprovador_id; }
+    public void setnome_fazenda(String nome_fazenda) { this.nome_fazenda = nome_fazenda; }
     public void setCultura(String cultura) { this.cultura = cultura; }
-    public void setProdutividadeAno(Double produtividadeAno) { this.produtividadeAno = produtividadeAno; }
+    public void setprodutividade_ano(Double produtividade_ano) { this.produtividade_ano = produtividade_ano; }
     public void setArea(Double area) { this.area = area; }
-    public void setTipoSolo(String tipoSolo) { this.tipoSolo = tipoSolo; }
+    public void settipo_solo(String tipo_solo) { this.tipo_solo = tipo_solo; }
     public void setCidade(String cidade) { this.cidade = cidade; }
     public void setEstado(String estado) { this.estado = estado; }
-    public void setVetorRaiz(String vetorRaiz) { this.vetorRaiz = vetorRaiz; }
-    public void setVetorAtualizado(String vetorAtualizado) { this.vetorAtualizado = vetorAtualizado; }
-    public void setVetorAprovado(String vetorAprovado) { this.vetorAprovado = vetorAprovado; }
+    public void setvetor_raiz(String vetor_raiz) { this.vetor_raiz = vetor_raiz; }
+    public void setvetor_atualizado(String vetor_atualizado) { this.vetor_atualizado = vetor_atualizado; }
+    public void setvetor_aprovado(String vetor_aprovado) { this.vetor_aprovado = vetor_aprovado; }
     public void setStatus(StatusArea status) { this.status = status; }
 }
--- porygon/src/main/java/fatec/porygon/entity/AreaAgricola.java
+++ porygon/src/main/java/fatec/porygon/entity/AreaAgricola.java
@@ -14,30 +14,30 @@ public class AreaAgricola {
 
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
-    private Usuario usuarioId;
+    private Usuario usuario_id;
 
     @ManyToOne
     @JoinColumn(name = "usuario_upgrade_id")
-    private Usuario usuarioUpgradeId;
+    private Usuario usuario_upgrade_id;
 
     @ManyToOne
     @JoinColumn(name = "usuario_aprovador_id")
-    private Usuario usuarioAprovadorId;
+    private Usuario usuario_aprovador_id;
 
     @Column(name = "nome_fazenda", nullable = false)
-    private String nomeFazenda;
+    private String nome_fazenda;
 
     @Column(name = "cultura", nullable = false)
     private String cultura;
 
     @Column(name = "produtividade_ano", nullable = false)
-    private Double produtividadeAno;
+    private Double produtividade_ano;
 
     @Column(name = "area", nullable = false)
     private Double area;
 
     @Column(name = "tipo_solo", nullable = false)
-    private String tipoSolo;
+    private String tipo_solo;
 
     @Column(name = "cidade", nullable = false)
     private String cidade;
@@ -46,13 +46,13 @@ public class AreaAgricola {
     private String estado;
 
     @Column(name = "vetor_raiz", nullable = false, columnDefinition = "JSON")
-    private String vetorRaiz;
+    private String vetor_raiz;
 
     @Column(name = "vetor_atualizado", columnDefinition = "JSON")
-    private String vetorAtualizado;
+    private String vetor_atualizado;
 
     @Column(name = "vetor_aprovado", columnDefinition = "JSON")
-    private String vetorAprovado;
+    private String vetor_aprovado;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
@@ -63,36 +63,36 @@ public class AreaAgricola {
         return id;
     }
 
-    public Usuario getUsuarioId() {
-        return usuarioId;
+    public Usuario getusuario_id() {
+        return usuario_id;
     }
 
     public Usuario getUsuarioUpgrade() {
-        return usuarioUpgradeId;
+        return usuario_upgrade_id;
     }
 
-    public Usuario getUsuarioAprovadorId() {
-        return usuarioAprovadorId;
+    public Usuario getusuario_aprovador_id() {
+        return usuario_aprovador_id;
     }
 
-    public String getNomeFazenda() {
-        return nomeFazenda;
+    public String getnome_fazenda() {
+        return nome_fazenda;
     }
 
     public String getCultura() {
         return cultura;
     }
 
-    public Double getProdutividadeAno() {
-        return produtividadeAno;
+    public Double getprodutividade_ano() {
+        return produtividade_ano;
     }
 
     public Double getArea() {
         return area;
     }
 
-    public String getTipoSolo() {
-        return tipoSolo;
+    public String gettipo_solo() {
+        return tipo_solo;
     }
 
     public String getCidade() {
@@ -103,16 +103,16 @@ public class AreaAgricola {
         return estado;
     }
 
-    public String getVetorRaiz() {
-        return vetorRaiz;
+    public String getvetor_raiz() {
+        return vetor_raiz;
     }
 
-    public String getVetorAtualizado() {
-        return vetorAtualizado;
+    public String getvetor_atualizado() {
+        return vetor_atualizado;
     }
 
-    public String getVetorAprovado() {
-        return vetorAprovado;
+    public String getvetor_aprovado() {
+        return vetor_aprovado;
     }
 
     public StatusArea getStatus() {
@@ -124,36 +124,36 @@ public class AreaAgricola {
         this.id = id;
     }
 
-    public void setUsuario(Usuario usuarioId) {
-        this.usuarioId = usuarioId;
+    public void setUsuario(Usuario usuario_id) {
+        this.usuario_id = usuario_id;
     }
 
-    public void setUsuarioUpgrade(Usuario usuarioUpgradeId) {
-        this.usuarioUpgradeId = usuarioUpgradeId;
+    public void setUsuarioUpgrade(Usuario usuario_upgrade_id) {
+        this.usuario_upgrade_id = usuario_upgrade_id;
     }
 
-    public void setUsuarioAprovador(Usuario usuarioAprovadorId) {
-        this.usuarioAprovadorId = usuarioAprovadorId;
+    public void setUsuarioAprovador(Usuario usuario_aprovador_id) {
+        this.usuario_aprovador_id = usuario_aprovador_id;
     }
 
-    public void setNomeFazenda(String nomeFazenda) {
-        this.nomeFazenda = nomeFazenda;
+    public void setnome_fazenda(String nome_fazenda) {
+        this.nome_fazenda = nome_fazenda;
     }
 
     public void setCultura(String cultura) {
         this.cultura = cultura;
     }
 
-    public void setProdutividadeAno(Double produtividadeAno) {
-        this.produtividadeAno = produtividadeAno;
+    public void setprodutividade_ano(Double produtividade_ano) {
+        this.produtividade_ano = produtividade_ano;
     }
 
     public void setArea(Double area) {
         this.area = area;
     }
 
-    public void setTipoSolo(String tipoSolo) {
-        this.tipoSolo = tipoSolo;
+    public void settipo_solo(String tipo_solo) {
+        this.tipo_solo = tipo_solo;
     }
 
     public void setCidade(String cidade) {
@@ -164,16 +164,16 @@ public class AreaAgricola {
         this.estado = estado;
     }
 
-    public void setVetorRaiz(String vetorRaiz) {
-        this.vetorRaiz = vetorRaiz;
+    public void setvetor_raiz(String vetor_raiz) {
+        this.vetor_raiz = vetor_raiz;
     }
 
-    public void setVetorAtualizado(String vetorAtualizado) {
-        this.vetorAtualizado = vetorAtualizado;
+    public void setvetor_atualizado(String vetor_atualizado) {
+        this.vetor_atualizado = vetor_atualizado;
     }
 
-    public void setVetorAprovado(String vetorAprovado) {
-        this.vetorAprovado = vetorAprovado;
+    public void setvetor_aprovado(String vetor_aprovado) {
+        this.vetor_aprovado = vetor_aprovado;
     }
 
     public void setStatus(StatusArea status) {
