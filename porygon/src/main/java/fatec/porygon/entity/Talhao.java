be8370e0601d5717806b5237c46dc388d0cba846
João Pedro <marcondesjpb@gmail.com>
2025-04-06 00:13:39 -0300

    feat(#44): Criação do entity talhao

---
+++ porygon/src/main/java/fatec/porygon/entity/Talhao.java
@@ -0,0 +1,39 @@
+package fatec.porygon.entity;
+
+import jakarta.persistence.*;
+
+    @Entity
+    @Table(name = "talhao")
+    public class Talhao {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(columnDefinition = "jsonb")
+        private String produtividadeAno;
+
+        private Double area;
+
+        @ManyToOne
+        @JoinColumn(name = "tipo_solo_id")
+        private tipoSolo tipoSolo;
+
+        @Column(name = "area_agricola_id")
+        private Long areaAgricolaId;
+
+        @Column(columnDefinition = "geometry")
+        private String arquivoDaninha;
+
+        @Column(columnDefinition = "geometry")
+        private String arquivoFinalDaninha;
+
+        @Column(columnDefinition = "jsonb")
+        private String arquivodaninha;
+
+        @Column(columnDefinition = "jsonb")
+        private String arquivofinaldaninha;
+
+        @OneToMany(mappedBy = "talhao")
+        private List<Safra> safras = new ArrayList<>();
+    }
+
