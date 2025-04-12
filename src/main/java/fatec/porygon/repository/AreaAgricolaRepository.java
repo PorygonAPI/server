c3c7cc7fd364906482d701c6bbb7cad98a96bb0a
Pablo <pablo.silva61@fatec.sp.gov.br>
2025-04-12 12:38:28 -0300

    feat (#46): add métodos de consulta no AreaAgricolaRepository para Cidade e StatusArea

--- src/main/java/fatec/porygon/repository/AreaAgricolaRepository.java
+++ src/main/java/fatec/porygon/repository/AreaAgricolaRepository.java
@@ -1,8 +1,13 @@
 package fatec.porygon.repository;
 
 import fatec.porygon.entity.AreaAgricola;
+import fatec.porygon.entity.Cidade;
+import fatec.porygon.enums.StatusArea;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AreaAgricolaRepository extends JpaRepository<AreaAgricola, Long> {
-    
+    List<AreaAgricola> findByCidade_id(Cidade cidade);
+    List<AreaAgricola> findByStatus(StatusArea status);
+    List<AreaAgricola> findByCidade_idAndStatus(Cidade cidade, StatusArea status);
 }
