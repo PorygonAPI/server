b418faf409dd386a5d301ac206f68cec19468001
RuthMira <143049504+RuthMira@users.noreply.github.com>
2025-04-12 22:20:25 -0300

    feat (#47): criar metodo no repository para dados unicos

--- src/main/java/fatec/porygon/repository/CulturaRepository.java
+++ src/main/java/fatec/porygon/repository/CulturaRepository.java
@@ -3,7 +3,9 @@ package main.java.fatec.porygon.repository;
 import fatec.porygon.entity.Cultura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
 public interface CulturaRepository extends JpaRepository<Cultura, Long> {
+    Optional<Cultura> findByNome(String nome);
 }
+
--- src/main/java/fatec/porygon/repository/TipoSoloRepository.java
+++ src/main/java/fatec/porygon/repository/TipoSoloRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TipoSoloRepository extends JpaRepository<TipoSolo, Long> {
+    Optional<TipoSolo> findByTipoSolo(String tipoSolo);
 }
