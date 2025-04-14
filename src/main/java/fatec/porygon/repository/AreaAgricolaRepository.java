70565080865cc2c08a980910a56b10d7cc54c9d3
Pablo <pablo.silva61@fatec.sp.gov.br>
2025-04-13 22:14:56 -0300

    fix(#46): retirando caps de letra

--- src/main/java/fatec/porygon/repository/AreaAgricolaRepository.java
+++ src/main/java/fatec/porygon/repository/AreaAgricolaRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AreaAgricolaRepository extends JpaRepository<AreaAgricola, Long> {
-    List<AreaAgricola> findByCidade_id(Cidade cidade);
+    List<AreaAgricola> findByCidadeId(Cidade cidade);
     List<AreaAgricola> findByStatus(StatusArea status);
-    List<AreaAgricola> findByCidade_idAndStatus(Cidade cidade, StatusArea status);
+    List<AreaAgricola> findByCidadeIdAndStatus(Cidade cidade, StatusArea status);
 }
