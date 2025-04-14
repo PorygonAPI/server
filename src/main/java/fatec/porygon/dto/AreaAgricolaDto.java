07aa900ee3ec201c9eca89643de940a94120e8d1
naira-maximo <naira.maximo@hotmail.com>
2025-04-14 00:22:17 -0300

    fix (#46): atualizando DTO para dar certo com JPA - método no service não aceita snake_case

--- src/main/java/fatec/porygon/dto/AreaAgricolaDto.java
+++ src/main/java/fatec/porygon/dto/AreaAgricolaDto.java
@@ -8,7 +8,7 @@ public class AreaAgricolaDto {
     private String estado;
     private StatusArea status;
     private String cidadeNome;
-    private String arquivo_fazenda;
+    private String arquivoFazenda;
 
     // Getters
     public Long getId() { return id; }
@@ -16,7 +16,7 @@ public class AreaAgricolaDto {
     public String getEstado() { return estado; }
     public StatusArea getStatus() { return status; }
     public String getCidadeNome() { return cidadeNome; }
-    public String getArquivoFazenda() { return arquivo_fazenda; }
+    public String getArquivoFazenda() { return arquivoFazenda; }
 
     // Setters
     public void setId(Long id) { this.id = id; }
@@ -24,5 +24,5 @@ public class AreaAgricolaDto {
     public void setEstado(String estado) { this.estado = estado; }
     public void setStatus(StatusArea status) { this.status = status; }
     public void setCidadeNome(String cidadeNome) { this.cidadeNome = cidadeNome; }
-    public void setArquivoFazenda(String arquivo_fazenda) { this.arquivo_fazenda = arquivo_fazenda; }
+    public void setArquivoFazenda(String arquivoFazenda) { this.arquivoFazenda = arquivoFazenda; }
 }
