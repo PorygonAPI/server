47c39dc671067ee2386289afb40bac5a243f0662
João Pedro <marcondesjpb@gmail.com>
2025-04-21 13:07:19 -0300

    feat(#60): Atualizando Table

--- banco/schema.sql
+++ banco/schema.sql
@@ -89,9 +89,9 @@ CREATE TABLE safra (
     usuario_analista_id INT,
     cultura_id INT,
     talhao_id INT,
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_ultima_versao DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (cultura_id) REFERENCES cultura(id),
     FOREIGN KEY (talhao_id) REFERENCES talhao(id),
-    FOREIGN KEY (usuario_analista_id) REFERENCES usuario(id),
-    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    data_ultima_versao DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP
+    FOREIGN KEY (usuario_analista_id) REFERENCES usuario(id)
 );
