c866e1e04e47902c95888c1c92b78b31b4a5dd6f
BispoJPM <marcondesjpb@gmail.com>
2025-05-15 14:11:13 -0300

    feat(#98): fix

--- src/main/java/fatec/porygon/service/RelatorioSafraService.java
+++ src/main/java/fatec/porygon/service/RelatorioSafraService.java
@@ -2,8 +2,13 @@ package fatec.porygon.service;
 
 import org.springframework.stereotype.Service;
 
+import fatec.porygon.dto.SafraRelatorioDto;
+import fatec.porygon.entity.Safra;
+import fatec.porygon.enums.StatusSafra;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class RelatorioSafraService {
@@ -16,6 +21,24 @@ public class RelatorioSafraService {
         long minutos = duracao.toMinutes() % 60;
 
         return String.format("%d dias, %d horas e %d minutos", dias, horas, minutos);
+    }
+
+    public List<SafraRelatorioDto> gerarRelatorioSafrasAprovadas() {
+        List<Safra> safrasAprovadas = safraRepository.findByStatus(StatusSafra.Aprovado);
+
+        return safrasAprovadas.stream().map(safra -> {
+            String tempo = calcularDuracao(
+                    safra.getDataCadastro(), safra.getDataAprovacao()
+            );
+
+            return new SafraRelatorioDto(
+                    safra.getId(),
+                    safra.getUsuarioAnalista().getNome(),
+                    safra.getDataCadastro(),
+                    safra.getDataAprovacao(),
+                    tempo
+            );
+        }).toList();
     }
 }
 
