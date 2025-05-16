b06c9e077f18e2969cb8039c25dae7f5c7bfaff3
Ruth mira <143049504+RuthMira@users.noreply.github.com>
2025-05-16 19:12:33 -0300

    feat(#109): criado service e controller para trazer os geojson

--- src/main/java/fatec/porygon/dto/SafraGeoJsonDto.java
+++ src/main/java/fatec/porygon/dto/SafraGeoJsonDto.java
@@ -4,43 +4,15 @@ import java.time.LocalDateTime;
 
 public class SafraGeoJsonDto {
     private String idSafra;
-    private String arquivoFazenda;
-    private String arquivoDaninha;
-    private String arquivoFinalDaninha;
     private LocalDateTime dataCadastro;
     private LocalDateTime dataUltimaVersao;
 
-    // Getters e Setters
     public String getIdSafra() {
         return idSafra;
     }
 
     public void setIdSafra(String idSafra) {
         this.idSafra = idSafra;
-    }
-
-    public String getArquivoFazenda() {
-        return arquivoFazenda;
-    }
-
-    public void setArquivoFazenda(String arquivoFazenda) {
-        this.arquivoFazenda = arquivoFazenda;
-    }
-
-    public String getArquivoDaninha() {
-        return arquivoDaninha;
-    }
-
-    public void setArquivoDaninha(String arquivoDaninha) {
-        this.arquivoDaninha = arquivoDaninha;
-    }
-
-    public String getArquivoFinalDaninha() {
-        return arquivoFinalDaninha;
-    }
-
-    public void setArquivoFinalDaninha(String arquivoFinalDaninha) {
-        this.arquivoFinalDaninha = arquivoFinalDaninha;
     }
 
     public LocalDateTime getDataCadastro() {
